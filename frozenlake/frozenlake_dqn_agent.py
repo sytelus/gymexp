8e3449e9f24ef6898d51265e3828d643ee69622a
Shital Shah <sytelus@gmail.com>
2019-11-05 15:47:37 -0800

    params

--- frozenlake/frozenlake_dqn_agent.py
+++ frozenlake/frozenlake_dqn_agent.py
@@ -13,8 +13,8 @@ ray.init(num_gpus=1)
 
 config = DEFAULT_CONFIG.copy()
 config.update({
-    "timesteps_per_iteration": 100,
-    "target_network_update_freq": 500,
+    "timesteps_per_iteration": 10,
+    "target_network_update_freq": 50,
     "buffer_size": 5000,
     "lr": 5e-3,
     "compress_observations": False,
@@ -22,7 +22,7 @@ config.update({
     "num_workers": 1,
     "num_gpus": 1,
     "noisy": False,
-    "exploration_fraction": 0.1,
+    "exploration_fraction": 0.5,
     "exploration_final_eps": 0.01,
     "schedule_max_timesteps": 200000
     })
@@ -34,7 +34,9 @@ def env_creator(env_config):
 register_env("frozenworld_env", env_creator)
 agent = DQNTrainer(config=config, env="frozenworld_env")
 
-for i in range(500):
+for i in range(50000):
     stats = agent.train()
     print(pretty_print(stats))
     print ('i, episode_reward_mean, episode_len_mean', i, stats['episode_reward_min'], stats['episode_len_mean'])
+    if stats['episode_reward_min'] > 0.0:
+        input("arrived?")
