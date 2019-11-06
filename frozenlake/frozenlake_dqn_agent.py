66e3b8a1e7fd223d153d32ccb76911742e60f8c3
Shital Shah <sytelus@gmail.com>
2019-11-05 16:12:19 -0800

    default params

--- frozenlake/frozenlake_dqn_agent.py
+++ frozenlake/frozenlake_dqn_agent.py
@@ -13,18 +13,18 @@ ray.init(num_gpus=1)
 
 config = DEFAULT_CONFIG.copy()
 config.update({
-    "timesteps_per_iteration": 100,
-    "target_network_update_freq": 50,
-    "buffer_size": 50000,
-    "lr": 5e-3,
+    # "timesteps_per_iteration": 100,
+    # "target_network_update_freq": 50,
+    # "buffer_size": 50000,
+    # "lr": 5e-3,
     "compress_observations": False,
-    "learning_starts": 100,
+    # "learning_starts": 100,
     "num_workers": 1,
-    "num_gpus": 1,
-    "noisy": False,
-    "exploration_fraction": 0.1,
-    "exploration_final_eps": 0.01,
-    "schedule_max_timesteps": 200000
+    "num_gpus": 1
+    # "noisy": False,
+    # "exploration_fraction": 0.1,
+    # "exploration_final_eps": 0.01,
+    # "schedule_max_timesteps": 200000
     })
 
 def env_creator(env_config):
