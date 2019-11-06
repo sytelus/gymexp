b8a8d4b992a5b15b9c203fc73c0ff0bbfd59c028
Shital Shah <sytelus@gmail.com>
2019-11-05 18:07:05 -0800

    More reward shaping

--- frozenlake/frozenlake_utils.py
+++ frozenlake/frozenlake_utils.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import gym
+import math
 from gym.envs.toy_text.frozen_lake import generate_random_map
 
 def seed(s):
@@ -10,10 +11,10 @@ def seed(s):
 def get_env(size, p=0.88, one_hot_obs=True, neg_dead_rew=True):
     random_map = generate_random_map(size=size, p=p)
     env = gym.make("FrozenLake-v0", desc=random_map)
+    if neg_dead_rew:
+        env = NegativeOnDeadWrapper(env)    
     if one_hot_obs:
         env = Int2OneHotWrapper(env)
-    if neg_dead_rew:
-        env = NegativeOnDeadWrapper(env)
     return env 
 
 
@@ -32,11 +33,22 @@ class Int2OneHotWrapper(gym.ObservationWrapper):
 class NegativeOnDeadWrapper(gym.Wrapper):
     def __init__(self, env):
         super(NegativeOnDeadWrapper, self).__init__(env)
+        self.ncols = self.nrows = int(math.sqrt(env.observation_space.n))
     
     def step(self, action):
         obs, reward, done, info = self.env.step(action)
         if reward == 0.0:
             # lower values for done will cause suicide tendency for agent
             # without else part, agent might just oscillate forever
-            reward = -1.0E4 if done else -1.0
+            if done:
+                reward = -1.0E4  
+            else:
+                col = obs % self.ncols
+                row = (obs - col) // self.nrows
+                dist = abs(self.ncols - col - 1) + abs(self.nrows -row -1)
+                dist_norm = float(dist) / (self.ncols + self.nrows) * 0.1
+                #print(obs, col, row, dist, dist_norm, self.ncols)
+                reward =  dist_norm
+
+                
         return obs, reward, done, info
